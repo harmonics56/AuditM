80957bacef45879fa80f34d90c6e90a519395a29
harmonics56 <86589780+harmonics56@users.noreply.github.com>
2022-09-13 12:13:23 +0530

    Delete Audit-Authorization directory

--- Audit-Authorization/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'varad', 'varadk', 'varad1234', 'Project-1');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Sri Harish', 'flash', 'sri1234', 'Project-2');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Kamalesh R', 'code', 'kamalesh1234', 'Project-3');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Megha S', 'megha', 'megha1234', 'Project-4');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Praduman Kumar', 'eren', 'praduman1234', 'Project-5');
