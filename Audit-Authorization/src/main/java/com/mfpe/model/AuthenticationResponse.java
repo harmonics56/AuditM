7897bdbb7a5ae20805b5f7c3afc76ac1842d9a11
harmonics56 <86589780+harmonics56@users.noreply.github.com>
2022-09-13 11:54:38 +0530

    Add files via upload

---
+++ Audit-Authorization/src/main/java/com/mfpe/model/AuthenticationResponse.java
@@ -0,0 +1,46 @@
+package com.mfpe.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Component
+public class AuthenticationResponse {
+	private String name;
+	private String projectName;
+	private boolean isValid;
+	
+	public AuthenticationResponse() {};
+	
+	public AuthenticationResponse(String name, String projectName, boolean isValid) {
+		super();
+		this.name = name;
+		this.projectName = projectName;
+		this.isValid = isValid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public boolean isValid() {
+		return isValid;
+	}
+	public void setValid(boolean isValid) {
+		this.isValid = isValid;
+	}
+	
+	
+}
